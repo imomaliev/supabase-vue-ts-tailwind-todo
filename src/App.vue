f3c8a15250463606bdc98a018b5de7009d3de0e5
Sardorbek Imomaliev <imomaliev@hey.com>
2021-08-02 12:28:32 +0700

    copy https://github.com/supabase/supabase/tree/master/examples/vue3-ts-todo-list sources and update them to match our project structure

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,67 @@
 <template>
-  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Tailwind" />
+  <div id="app" class="w-full h-full flex flex-col justify-center bg-gray-300">
+    <div
+      v-if="showPasswordReset"
+      class="w-full h-full flex flex-col justify-center items-center p-4"
+    >
+      <PasswordReset />
+    </div>
+    <div
+      v-else-if="userSession === null"
+      class="w-full h-full flex flex-col justify-center items-center p-4"
+    >
+      <Auth />
+    </div>
+    <div v-else class="w-full h-full flex flex-col justify-center items-center p-4 max-w-sm m-auto">
+      <Suspense>
+        <template #default>
+          <div>
+            <TodoList />
+            <button class="btn-black w-full mt-12" @click="handleLogout">
+              Logout
+            </button>
+          </div>
+        </template>
+        <template #fallback>
+          <Loading />
+        </template>
+      </Suspense>
+    </div>
+    <Footer />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+<script>
+import Auth from '@/components/Auth.vue'
+import PasswordReset from '@/components/PasswordReset.vue'
+import TodoList from '@/components/TodoList.vue'
+import Loading from '@/components/Loading.vue'
+import Footer from '@/components/Footer.vue'
+import { userSession, handleLogout } from '@/utils/useAuth'
+import { getParameterByName } from '@/lib/helpers'
 
-export default defineComponent({
-  name: 'App',
+export default {
   components: {
-    HelloWorld,
+    Auth,
+    PasswordReset,
+    TodoList,
+    Loading,
+    Footer,
   },
-})
+  computed: {
+    showPasswordReset: function() {
+      const requestType = getParameterByName('type', location.href)
+      return requestType === 'recovery'
+    }
+  },
+  setup() {
+    return { userSession, handleLogout }
+  },
+}
 </script>
 
 <style>
 #app {
-  @apply antialiased text-center text-default m-16;
+  @apply h-screen min-h-screen antialiased;
 }
 </style>
